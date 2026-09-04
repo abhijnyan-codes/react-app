eaef4901f24a61fe55681f0c517652449cf0e5ac
Abhijnyan saikia <abhijnyansaikia@zohomail.in>
2026-09-04 16:07:00 +0530

    Update command history

--- command.sh
+++ command.sh
@@ -1,11 +1,21 @@
+npx create-react-app react-app
+
 git status
+
+gh repo create react-app --public --source=. --remote=origin --push
+
 git checkout -b update_logo
+
 git add src/App.js src/App.css
 git commit -m "Update logo and link"
 git push -u origin update_logo
-gh repo create react-app --public --source=. --remote=origin --push
+
 git checkout main
 git checkout -b master
 git push -u origin master
+
 gh pr create --base master --head update_logo --title "Update logo and link" --body "Updated the logo and changed the link to the Propeller Dirtmate page."
+
 gh pr merge update_logo --merge
+
+# REPO_URL https://github.com/abhijnyan-codes/react-app
